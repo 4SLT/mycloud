70db39fd73402111b46d92b05568f12c4f735f4e
zongxiong Lin <zongxiong.lin@ucarinc.com>
2020-05-19 17:38:51 +0800

    ADD canal

---
+++ mycloud-server/src/main/java/com/fslt/mycloudserver/modules/canal/CanalTest.java
@@ -0,0 +1,99 @@
+package com.fslt.mycloudserver.modules.canal;
+
+import com.alibaba.otter.canal.client.CanalConnector;
+import com.alibaba.otter.canal.client.CanalConnectors;
+import com.alibaba.otter.canal.protocol.CanalEntry.Column;
+import com.alibaba.otter.canal.protocol.CanalEntry.Entry;
+import com.alibaba.otter.canal.protocol.CanalEntry.EntryType;
+import com.alibaba.otter.canal.protocol.CanalEntry.EventType;
+import com.alibaba.otter.canal.protocol.CanalEntry.RowChange;
+import com.alibaba.otter.canal.protocol.CanalEntry.RowData;
+import com.alibaba.otter.canal.protocol.Message;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+/**
+ * @description: CanalTest
+ * @date: 2020/5/19 16:19
+ * @author: 林宗雄
+ * @version: 1.0
+ */
+public class CanalTest {
+
+    public static void main(String[] args) {
+        CanalConnector canalConnector = CanalConnectors.newSingleConnector(new InetSocketAddress("47.100.138.188", 11111), "example", "canal", "canal");
+        int batchSize = 1000;
+        int emptyCount = 0;
+        try {
+            canalConnector.connect();
+            canalConnector.subscribe(".*\\..*");
+            canalConnector.rollback();
+            int totalEmptyCount = 1200;
+            while (emptyCount < totalEmptyCount) {
+                //获取指定数量的数据
+                Message message = canalConnector.getWithoutAck(batchSize);
+                long batchId = message.getId();
+                int size = message.getEntries().size();
+                if (batchId == -1 || size == 0) {
+                    emptyCount++;
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    emptyCount = 0;
+                    printEntry(message.getEntries());
+                }
+                //提交确认
+                canalConnector.ack(batchId);
+            }
+            System.out.println("empty too many times, exit");
+        } finally {
+            canalConnector.disconnect();
+        }
+    }
+
+    private static void printEntry(List<Entry> entrys) {
+        for (Entry entry : entrys) {
+            if (entry.getEntryType() == EntryType.TRANSACTIONBEGIN
+                    || entry.getEntryType() == EntryType.TRANSACTIONEND) {
+                continue;
+            }
+
+            RowChange rowChage = null;
+            try {
+                rowChage = RowChange.parseFrom(entry.getStoreValue());
+            } catch (Exception e) {
+                throw new RuntimeException("ERROR ## parser of eromanga-event has an error , data:" + entry.toString(),
+                        e);
+            }
+
+            EventType eventType = rowChage.getEventType();
+            System.out.println(String.format("================> binlog[%s:%s] , name[%s,%s] , eventType : %s",
+                    entry.getHeader().getLogfileName(), entry.getHeader().getLogfileOffset(),
+                    entry.getHeader().getSchemaName(), entry.getHeader().getTableName(), eventType));
+
+            for (RowData rowData : rowChage.getRowDatasList()) {
+                if (eventType == EventType.DELETE) {
+                    printColumn(rowData.getBeforeColumnsList());
+                } else if (eventType == EventType.INSERT) {
+                    printColumn(rowData.getAfterColumnsList());
+                } else {
+                    System.out.println("-------> before");
+                    printColumn(rowData.getBeforeColumnsList());
+                    System.out.println("-------> after");
+                    printColumn(rowData.getAfterColumnsList());
+                }
+            }
+        }
+    }
+
+    private static void printColumn(List<Column> columns) {
+        for (Column column : columns) {
+            System.out.println(column.getName() + " : " + column.getValue() + "    update=" + column.getUpdated());
+        }
+    }
+
+}
